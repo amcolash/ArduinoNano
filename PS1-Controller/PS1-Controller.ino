cd122952a528d91289e5c7a59a05c54a1df6eddf
Andrew McOlash <amcolash@gmail.com>
2021-11-04 19:33:55 -0700

    First working version w/ ps1 input and valid output

--- PS1-Controller/PS1-Controller.ino
+++ PS1-Controller/PS1-Controller.ino
@@ -1,20 +1,39 @@
+#include <DigitalIO.h>
+#include <PsxControllerBitBang.h>
 #include <SPI.h>
 
-#define ACK 9
+#define LED_PIN 2
 
-uint16_t buttons = 0;
-unsigned long timer;
+// Pins to be used for output to ps1
+#define ACK_PIN 9
+
+// Pins to be used for input from controller
+#define ATT_PIN 4
+#define CMD_PIN 5
+#define DAT_PIN 6
+#define CLK_PIN 7
+
+PsxControllerBitBang<ATT_PIN, CMD_PIN, DAT_PIN, CLK_PIN> psx;
+
+uint16_t buttons;
+byte lx, ly, rx, ry;
+
+bool haveController = false;
 
 void setup (void) {
-  Serial.begin (115200);   // debugging
+  Serial.begin (115200);
 
+  // Setup output (to ps1) pins
   pinMode(SCK, INPUT);
   pinMode(MOSI, INPUT);
   pinMode(MISO, OUTPUT);
   pinMode(SS, INPUT);
-  pinMode(ACK, OUTPUT);
 
-  digitalWrite(ACK, HIGH);
+  fastPinMode(ACK_PIN, OUTPUT);
+  fastDigitalWrite(ACK_PIN, HIGH);
+
+  fastPinMode(LED_PIN, OUTPUT);
+  fastDigitalWrite(LED_PIN, LOW);
   
   // turn on SPI in slave mode
   SPCR = _BV(SPE) | _BV(DORD) | _BV(SPR0) | ~_BV(MSTR);
@@ -33,9 +52,9 @@ byte sendByte(byte b, bool sendAck) {
   
   if (sendAck && ret != 0x43) {
     delayMicroseconds(4); // Need to wait for last clock pulse before acking
-    digitalWrite(ACK, LOW);
+    fastDigitalWrite(ACK_PIN, LOW);
     delayMicroseconds(2);
-    digitalWrite(ACK, HIGH);
+    fastDigitalWrite(ACK_PIN, HIGH);
   }
   
   return ret;
@@ -54,8 +73,13 @@ void printByte(byte b) {
   Serial.println("]");
 }
 
-void loop (void) {
+void updateButtons() {
+  buttons = ~psx.getButtonWord();
+  psx.getLeftAnalog (lx, ly);
+  psx.getRightAnalog (rx, ry);
+}
 
+void loop (void) {
   // Get next command
   byte cmd = sendByte(0xFF);
   if (cmd == 0x01) cmd = sendByte(0x73, true);
@@ -64,19 +88,20 @@ void loop (void) {
     case 0x42: // Poll Buttons
       sendByte(0x5A); // Tell PS1 we are sending poll update
 
-      sendByte(buttons >> 8); // Buttons part 1
-      sendByte((buttons << 8) >> 8); // Buttons part 2
+      sendByte((buttons << 8) >> 8); // Buttons part 1
+      sendByte(buttons >> 8); // Buttons part 2
       
-      sendByte(0x80); // Right Joy X-Axis
-      sendByte(0x80); // Right Joy Y-Axis
-      sendByte(0x80); // Left Joy X-Axis
-      sendByte(0x80, false); // Left Joy Y-Axis
+      sendByte(rx); // Right Joy X-Axis
+      sendByte(ry); // Right Joy Y-Axis
+      sendByte(lx); // Left Joy X-Axis
+      sendByte(ly, false); // Left Joy Y-Axis
       
       break;
     default:
       Serial.print("Unknown Command: ");
       printByte(cmd);
     case 0x00: // NOP?
+    case 0x01: // Init (should have already been handled...)
     case 0x43: // Config Mode (But also poll)
     case 0x4D: // Enable Rumble
     case 0xFF: // NOP?
@@ -84,10 +109,21 @@ void loop (void) {
       break;
   }
 
-  if (millis() - timer > 100) {
-    if (buttons == 0) buttons = 65535;
-    else buttons /= 2;
+  if (!haveController) {
+    if (psx.begin()) {
+      Serial.println("Found controller");
+      haveController = true;
 
-    timer = millis();
+      updateButtons();
+    }
+  } else {
+    if (!psx.read()) {
+      Serial.println("Lost controller");
+      haveController = false;
+    } else {
+      updateButtons();
+    }
   }
+
+  fastDigitalWrite(LED_PIN, haveController);
 }
